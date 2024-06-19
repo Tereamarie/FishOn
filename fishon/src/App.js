d31dcbdf24875812766d306131d3941b92f4c8a8
Marie Knowlton <mtknowlton67@gmail.com>
2024-06-19 12:37:09 -0700

    added react componets added home componets

---
+++ fishon/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from './components/Home/Home.js';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Home />
+      </header>
+    </div>
+  );
+}
+
+export default App;
