ec622d0eb3e652bb9391532c440db087494d2a63
Marie Knowlton <mtknowlton67@gmail.com>
2024-06-28 10:49:52 -0700

    refactored css file added code for header and top bar

--- fishon/src/App.js
+++ fishon/src/App.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import Home from './Components/Home/home.js';
+import React from "react";
 import Header from './Components/Header.js';
-import './App.css';
-import './CSS/index.css';
-function App() {
-  return (
-    <div className="App">
-      
-       <Home />
-      {/* <Header /> */}
-    </div>
-  );
-}
+import TopBar from './Components/TopBar.js';
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <TopBar />
+  </div>
+);
 
 export default App;
--- fishon/src/Components/Header.js
+++ fishon/src/Components/Header.js
@@ -1,20 +1,23 @@
-// import React from "react";
+import React from "react";
 
 // Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
-// const Header = () => {
-//   const current = new Date();
-//   const date = `${current.getDate()}/${
-//     current.getMonth() + 1
-//   }/${current.getFullYear()}`;
-
-//   return (
-//     <div className="header">
-//       <span className="date">{date}</span><br></br>
-//       <span className="temp">98°</span>
-//     </div>
-//   );
-// };
-
-// export default Header;
+const Header = () => {
+    const current = new Date();
+    const date = `${current.getDate()}/${
+      current.getMonth() + 1
+    }/${current.getFullYear()}`;
+  
+    return (
+      <div className="header">
+        <span className="date">{date}</span>
+  
+        <h1>FishOn!<th></th>Fishing App</h1>
+        <span className="temp">98°</span>
+      </div>
+    );
+  };
+  
+  export default Header;
+  
